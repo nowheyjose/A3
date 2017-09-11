f9e7fbb158906a5b970472d9aa5745fd49a01a95
Jose-Rafael <jdiaz5511@gmail.com>
2017-09-10 20:49:46 -0400

    updated assignment

--- ListingSchema.js
+++ ListingSchema.js
@@ -1,15 +1,29 @@
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 /* Create your schema */
 var listingSchema = new Schema({
   /* your code here */
+  code: String,
+  name: String,
+  coordinates: {
+    latitude: Number,
+    longitude: Number
+  },
+  address: String
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
   /* your code here */
+  var currDate = new Date();
+  this.updated_at = currDate;
+
+  if(!this.created_at)
+    this.created_at = currDate;
+
+  next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
--- queries.js
+++ queries.js
@@ -1,28 +1,59 @@
 /* Fill out these functions using Mongoose queries*/
 
 var findLibraryWest = function() {
-  /* 
+  /*
     Find the document that contains data corresponding to Library West,
-    then log it to the console. 
+    then log it to the console.
    */
+   Listing.find({ name: 'Library West' }, function(err, bldg) {
+  if (err) throw err;
+
+  // object of the user
+  console.log(bldg);
+  };
 };
 var removeCable = function() {
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
+   Listing.findOneAndRemove({ code: 'CABL' }, function(err) {
+  if (err) throw err;
+
+  // we have deleted the user
+  console.log('document deleted!');
+  };
 };
 var updatePhelpsLab = function() {
   /*
-    Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
+    Phelps Laboratory's address is incorrect. Find the listing, update it, and then
+    log the updated document to the console.
    */
+   Listing.find({ name: 'Phelps Laboratory'}, function(err, bldg) {
+     if (err) throw err;
+
+  // change the users location
+    Listing.address = '1234 Address Ave.';
+
+  // save the user
+  Listing.save(function(err) {
+    if (err) throw err;
+
+    console.log('Building updated!');
+    };
+  };
 };
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
+  /*
+    Retrieve all listings in the database, and log them to the console.
    */
+   Listing.find({}, function(err, bldgs) {
+  if (err) throw err;
+
+  // object of all the users
+  console.log(bldgs);
+};
 };
 
 findLibraryWest();
